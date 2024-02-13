476b0196046f15a12742ce11e6cfbba11052e231
Kalin Toshev <kalin.toshev84@gmail.com>
2024-02-13 02:28:27 +0200

    Allow user to input course names and decide whether to continue adding courses
    This commit adds functionality to allow the user to input course names interactively. The program prompts the user to enter the name of each course and provides an option to continue adding more courses or finish inputting. The entered course names are stored in the courseNames vector for later use in the CGPA calculation.

--- cgpa_calculator.cpp
+++ cgpa_calculator.cpp
@@ -9,6 +9,23 @@ int main()
     vector<string> courseNames;
     vector<char> grades;
     vector<int> creditHours;
+    char continueAdding;
+
+    //Ask user to enter course names
+    do
+    {
+        string courseName;
+        cout << "Enter course name: ";
+        getline(cin, courseName);
+        courseNames.push_back(courseName);
+
+        cout << "Do you want to add another course? (y/n): ";
+        cin >> continueAdding;
+
+        //Ignore newline character from previous input
+        cin.ignore();
+    } while (tolower(continueAdding) == 'y');
+    
 
     return 0;
 }
